26f76ad1f6440d3f5777d36f774ab30944dd7d79
misery1x <brisonbuilder123@proton.me>
2025-05-06 09:43:10 -0400

    Update camera.hpp

--- camera.hpp
+++ camera.hpp
@@ -1,26 +1,31 @@
-struct FNRot
-{
-	double a;
-	double b;
-	double c;
-};
+#pragma once
+
+#define M_PI 3.14159265358979323846
 
 camera_position_s camera_class::GetCamera()
 {
 	camera_position_s camera;
 
-  uintptr_t location_pointer = memory.Read<uintptr_t>(cache.system.UWorld + 0x148);
-  uintptr_t rotation_pointer = memory.Read<uintptr_t>(cache.system.UWorld + 0x158);
+	auto location_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x160);
+	auto rotation_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x170);
 
-  FNRot fnrot{};
-  fnrot.a = memory.Read<double>(rotation_pointer);
-  fnrot.b = memory.Read<double>(rotation_pointer + 0x20);
-  fnrot.c = memory.Read<double>(rotation_pointer + 0x1D0);
+	struct FNRot
+	{
+		double a; //0x0000
+		char pad_0008[24]; //0x0008
+		double b; //0x0020
+		char pad_0028[424]; //0x0028
+		double c; //0x01D0
+	} fnRot;
 
-  camera.location = memory.Read<FVector>(location_pointer);
-  camera.rotation.x = asin(fnrot.c) * (180.0 / M_PI);
-  camera.rotation.y = ((atan2(fnrot.a * -1, fnrot.b) * (180.0 / M_PI)) * -1) * -1;
-  camera.fov = memory.Read<float>(cache.system.PlayerController + 0x3AC) * 90.0f;
-  
+	fnRot.a = memory.read<double>(rotation_pointer);
+	fnRot.b = memory.read<double>(rotation_pointer + 0x20);
+	fnRot.c = memory.read<double>(rotation_pointer + 0x1d0);
+
+	camera.location = memory.read<FVector>(location_pointer);
+	camera.rotation.x = asin(fnRot.c) * (180.0 / M_PI);
+	camera.rotation.y = ((atan2(fnRot.a * -1, fnRot.b) * (180.0 / M_PI)) * -1) * -1;
+	camera.fov = memory.read<float>((uintptr_t)cache.system.PlayerController + 0x3AC) * 90.f;
+
 	return camera;
 }
