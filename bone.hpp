d7f6b331b8c7f9634d4c65e7ef2a26bf29a95878
misery1x <brisonbuilder123@proton.me>
2025-05-17 17:08:30 -0400

    Update bone.hpp

--- bone.hpp
+++ bone.hpp
@@ -1,9 +1,11 @@
+#pragma once
+
 FVector sdk_bones_class::GetBoneLocation(uintptr_t Mesh, int bone_index)
 {
-	uintptr_t bone_array = memory.Read<uintptr_t>(Mesh + offsets::BoneArray);
-	if (bone_array == 0) bone_array = memory.Read<uintptr_t>(Mesh + offsets::BoneCache);
-	FTransform bone = memory.Read<FTransform>(bone_array + (bone_index * 0x60));
-	FTransform component_to_world = memory.Read<FTransform>(Mesh + offsets::ComponentToWorld);
+	uintptr_t bone_array = memory.read<uintptr_t>(Mesh + offsets::BoneArray);
+	if (bone_array == 0) bone_array = memory.read<uintptr_t>(Mesh + offsets::BoneCache);
+	FTransform bone = memory.read<FTransform>(bone_array + (bone_index * 0x60));
+	FTransform component_to_world = memory.read<FTransform>(Mesh + offsets::ComponentToWorld);
 	D3DMATRIX Matrix = matrix.MatrixMultiplication(bone.ToMatrixWithScale(), component_to_world.ToMatrixWithScale());
 	return FVector(Matrix._41, Matrix._42, Matrix._43);
 }
