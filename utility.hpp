84bbb5f76a445d51712bbeac55abb06779d2b486
misery1x <brisonbuilder123@proton.me>
2025-05-17 17:07:45 -0400

    Update utility.hpp

--- utility.hpp
+++ utility.hpp
@@ -1,6 +1,8 @@
-bool sdk_utility_class::IsEnemyVisible(uintptr_t Mesh)
+#pragma once
+
+bool sdk_utility_class::IsEnemyVisible(uintptr_t Mesh_MJ)
 {
 	auto Seconds = memory.read<double>(cache.system.UWorld + 0x190);
-	auto LastRenderTime = memory.read<float>(Mesh + 0x32C);
+	auto LastRenderTime = memory.read<float>(Mesh_MJ + 0x32C);
 	return Seconds - LastRenderTime <= 0.06f;
 }
