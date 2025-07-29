98b23da834c24065fdad54238c82e6392d403d64
misery1x <brisonbuilder123@proton.me>
2025-07-29 19:45:10 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -4,8 +4,8 @@
 
 namespace offsets
 {
-	inline uintptr_t UWorld = 0x185565C8;
-	inline uintptr_t OwningGameInstance = 0x248;
+	inline uintptr_t UWorld = 0x173AF320;
+	inline uintptr_t OwningGameInstance = 0x250;
 	inline uintptr_t LocalPlayers = 0x38;
 	inline uintptr_t PlayerController = 0x30;
 	inline uintptr_t AcknowledgedPawn = 0x350;
@@ -13,7 +13,7 @@ namespace offsets
 	inline uintptr_t PlayerState = 0x2C8;
 	inline uintptr_t TeamIndex = 0x1291;
 	inline uintptr_t RootComponent = 0x1B0;
-	inline uintptr_t GameState = 0x1D0;
+	inline uintptr_t GameState = 0x1D8;
 	inline uintptr_t PlayerArray = 0x2C0;
 	inline uintptr_t PawnPrivate = 0x320;
 
@@ -27,15 +27,14 @@ namespace offsets
 	inline uintptr_t Velocity = 0x180;
 
 	inline uintptr_t CurrentWeapon = 0xAF0;
-	inline uintptr_t WeaponData = 0x6E8;
-	inline uintptr_t AmmoCount = 0x127C;
+	inline uintptr_t WeaponData = 0x560;
+	inline uintptr_t AmmoCount = 0x121C;
 	inline uintptr_t ItemName = 0x40;
 	inline uintptr_t Tier = 0xa2; // EFortRarity  Rarity
 
 	inline uintptr_t Platform = 0x430;
 
 	// Rank
-	inline uintptr_t HabaneroComponent = 0x5490;
-	inline uintptr_t RankedProgress = 0xd0;
+	inline uintptr_t HabaneroComponent = 0xA80;
+	inline uintptr_t RankedProgress = 0xD0;
 }
-
