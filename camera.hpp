3d014a44b32a4c0fe1167ad899ae4b72b44a775c
misery1x <brisonbuilder123@proton.me>
2025-08-24 16:06:13 -0400

    Update camera.hpp

--- camera.hpp
+++ camera.hpp
@@ -6,8 +6,8 @@ camera_position_s camera_class::GetCamera()
 {
 	camera_position_s camera;
 
-	auto location_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x170); // Rotation is always location + 0x10
-	auto rotation_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x180);
+	auto location_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x178); // Rotation is always location + 0x10
+	auto rotation_pointer = memory.read<uintptr_t>(cache.system.UWorld + 0x188);
 
 	struct FNRot
 	{
